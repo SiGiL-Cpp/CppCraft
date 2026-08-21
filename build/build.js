7952cc37689699aaa8007d4641c67fa90d4fd25d
SiGiL-Cpp <groslemesre.stephane@gmail.com>
2026-08-21 20:50:38 +0100

    Font fix + illu 01

--- build/build.js
+++ build/build.js
@@ -260,6 +260,7 @@ function buildShell(meta, articleHtml, navHtml, pageId) {
 </head>
 <body data-page-id="${escHtml(pageId)}">
   <header class="site-header">
+    <a href="index.html" class="site-brand">C++<span class="site-brand-acc">Craft</span></a>
     <nav class="lesson-nav" id="lesson-nav"></nav>
     <button class="theme-toggle" id="theme-toggle" onclick="toggleTheme()">◑ Dark</button>
   </header>
